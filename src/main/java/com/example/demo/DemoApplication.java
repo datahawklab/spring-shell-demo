f70b12d5f4df9130c2ba1eb0188f01635673b136
swapan-datahawklab <swapan.chakrabarty@datahawklab.com>
2025-03-14 01:13:49 -0400

    done

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.command.annotation.CommandScan;
 
 @SpringBootApplication
+@CommandScan
 public class DemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/commands/HelloCommand.java
+++ src/main/java/com/example/demo/commands/HelloCommand.java
@@ -1,13 +1,18 @@
 package com.example.demo.commands;
 
-import org.springframework.shell.standard.ShellComponent;
-import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.command.annotation.Command;
+import org.springframework.shell.command.annotation.Option;
 
-@ShellComponent
+@Command(command = "HelloCommand", group = "Hello Commands")
 public class HelloCommand {
 
-    @ShellMethod(key = "hello", value = "This command prints hello")
-    public String hello() {
-        return "Hello!";
+    @Command(command = "describe", description = "Describe XYZ that is passed")
+    public String describe(@Option(longNames = {"xyz-arg"}, required = true) String xyzArg) {
+        return "Hello world " + xyzArg;
+    }
+
+    @Command(command = "list", description = "List all foos")
+    public String list() {
+        return "Listy McListface";
     }
 }
